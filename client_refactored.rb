a43b38a4b86bda880ddf05adbcc0841a2c5d4b1a
Henry Maddocks <hmaddocks@me.com>
2021-11-30 22:16:36 +0000

    Template methos pattern with blocks

--- client_refactored.rb
+++ client_refactored.rb
@@ -1,10 +1,49 @@
-require_relative "./report"
-require_relative "./txt_report"
-require_relative "./html_report"
-require_relative "./json_report"
+require "date"
+require "json"
+
+class Report
+  class << self
+    def call
+      yield header, footer
+    end
+
+    private
+
+    def header = "#{year} Report"
+    def footer = "#{year} @ Copyright Acme Inc."
+    def spacer = "#{'-' * 72}"
+    def year = Date.today.year.to_s
+  end
+end
 
 data = "This is some data I want to put in my report"
 
-# puts TXTReport.call(data)
-# puts HTMLReport.call(data)
-puts JSONReport.call(data)
+html = Report.call() do |header, footer|
+  <<~HTML
+    <!DOCTYPE html>
+    <head><title>#{header}</title></head>
+    <html>
+      <body>#{data}</body>
+      <footer>#{footer}</footer>
+    </html>
+  HTML
+end
+
+json = Report.call() do |header, footer|
+  { title: header, body: data, footer: footer }.to_json
+end
+
+txt = Report.call() do |header, footer|
+  <<~END
+    #{header}
+    "#{'-' * 72}"
+    #{data}
+    "#{'-' * 72}"
+    #{footer}
+  END
+end
+
+puts html
+puts json
+puts txt
+
